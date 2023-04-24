dacb9f6c72aea1b6dc3f470f022a17df164d9912
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-04-24 11:36:49 +0300

    validation

--- src/components/ContactsForm/ContactsForm.js
+++ src/components/ContactsForm/ContactsForm.js
@@ -1,6 +1,20 @@
 import { Formik } from 'formik';
 import { FormField, Form, Button, Field } from './ContactsForm.styled';
 import { FormWrapper } from './ContactsForm.styled';
+import * as Yup from 'yup';
+
+const ContactsSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required field'),
+  number: Yup.number()
+    .typeError("That doesn't look like a phone number")
+    .positive("A phone number can't start with a minus")
+    .integer("A phone number can't include a decimal point")
+    .min(8)
+    .required('Required field'),
+});
 
 export const ContactsForm = () => (
   <FormWrapper>
@@ -10,8 +24,10 @@ export const ContactsForm = () => (
         name: '',
         number: '',
       }}
+      validationSchema={ContactsSchema}
       onSubmit={values => {
         console.log(values);
+        console.log(values.number);
       }}
     >
       <Form>
